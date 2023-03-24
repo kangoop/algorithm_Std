6b5c8d072d31cc79eceee2edbad5cc33fe99b5d4
kangoop000 <kangoop000@gmail.com>
2023-03-24 23:05:52 +0900

    no message

--- hwkang/algorithom_problem_solution/Temp_Project/my_algorithm_lib/obj/Debug/netstandard2.0/total_lib.AssemblyInfo.cs
+++ hwkang/algorithom_problem_solution/Temp_Project/my_algorithm_lib/obj/Debug/netstandard2.0/total_lib.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("my_algorithm_lib")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("total_lib")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("my_algorithm_lib")]
-[assembly: System.Reflection.AssemblyTitleAttribute("my_algorithm_lib")]
+[assembly: System.Reflection.AssemblyProductAttribute("total_lib")]
+[assembly: System.Reflection.AssemblyTitleAttribute("total_lib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment 클래스에서 생성되었습니다.
