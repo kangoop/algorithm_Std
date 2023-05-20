6c6584e20ab49fb8dca8b241fcd0068dc87284e9
Kangoop <kangoop000@gmail.com>
2023-05-21 02:20:54 +0900

    트리 진입

--- hwkang/algorithom_problem_solution/Temp_Project/Temp_project_netframework/Program.cs
+++ hwkang/algorithom_problem_solution/Temp_Project/Temp_project_netframework/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.Security.Principal;
 using System.Text;
 
@@ -20,81 +22,189 @@ namespace Temp_project_netframework
 
         static void Main(string[] args)
         {
-            int InputData_int = InputData_Step();
+            int node_cnt = InputData_Step();
 
-            int[] InputData = InputData_Step2(InputData_int);
+            MyNode[] mynodes = new MyNode[node_cnt];
 
-            int[] output_data = Counting_Sort(InputData, new int[InputData.Length], InputData.Length);
+            Process_Step(node_cnt,mynodes);
 
-            OutputData_Step(output_data);
+            OutputData_Step(mynodes);
         }
 
         static int InputData_Step()
         {
-            string input = Console.ReadLine();
+            string node_cnt = Console.ReadLine();
 
-            return int.Parse(input);
+            return int.Parse(node_cnt);
         }
 
-        static int[] InputData_Step2(int cnt)
+
+        static void Process_Step(int node_cnt, MyNode[] mynodes)
         {
-            string[] input = Console.ReadLine().Split(' ');
-
-            int[] output = new int[input.Length];
-            for(int i=0; i < cnt; i++)
+            for (int i = 0; i < node_cnt; i++)
             {
-                output[i] = int.Parse(input[i]); 
-            }
+                string[] node_info = Console.ReadLine().Replace(" ", ",").Split(',');
 
-            return output;
+                string node_id = node_info[0];
+               
+                if (mynodes[int.Parse(node_id)] == null)
+                {
+                    MyNode node = new MyNode();
+                    mynodes[int.Parse(node_id)] = node;
+                    mynodes[int.Parse(node_id)].value = node_id;
+                }
+
+                MyNode parentnode = mynodes[int.Parse(node_id)];
+
+
+                for (int j = 2; j < node_info.Length; j++) // childnode 추가 
+                {
+                    string childnode_id = node_info[j];
+
+                    
+                    if (mynodes[int.Parse(childnode_id)] == null)
+                    {
+                        MyNode childnode = new MyNode();
+
+                        mynodes[int.Parse(childnode_id)] = childnode;
+
+                        childnode.value  = childnode_id;
+
+                        childnode.AddParentnode(parentnode);
+
+                        parentnode.Addchildnode(childnode);
+
+                    }
+                    else
+                    {
+                        MyNode childnode = mynodes[int.Parse(childnode_id)];
+                        childnode.AddParentnode(parentnode);
+                        parentnode.Addchildnode(childnode);
+
+                    }
+                }
+            }
         }
 
-        static int[] Counting_Sort(int[] data_arr, int[] output_arr,int maxsize)
+
+        static void OutputData_Step(MyNode[] mynodes)
         {
-            int[] count_arr = new int[10_001];
-
-            for(int i = 0; i < maxsize; i++)
+            foreach (var node in mynodes)
             {
-                int value = data_arr[i];
-
-                count_arr[value]++;
+                Console.WriteLine($"node {node.value}: parent = {node.GetParentNodeId()}, depth = {node.NodeDepth()}, {node.Nodetype()}, {node.Childnode()}");
             }
-
-
-            for(int i = 1; i <= count_arr.Length-1; i++)
-            {
-                count_arr[i] = count_arr[i] + count_arr[i - 1];
-            }
-
-            for(int j= maxsize - 1; j >= 0; j--)
-            {
-                var item = data_arr[j];
-                var count_idx = count_arr[item];
-                output_arr[count_idx - 1] = item;
-                count_arr[item]--;
-            }
-
-            return output_arr;
         }
 
 
+    }
 
-        static void OutputData_Step(int[] output)
+    public class MyNode
+    {
+        MyNode parentnode;
+        List<MyNode> childnodes = new List<MyNode>();
+        //MyNode[] childnodes = new MyNode[0] { };
+        public string value { get; set; }// node id 
+
+        public MyNode()
+        {
+            
+        }
+
+        public string GetParentNodeId()
+        {
+            return parentnode == null ? "-1" : parentnode.value;
+        }
+
+        public void AddParentnode(MyNode node)
+        {
+            if(value == node.value)
+            {
+                parentnode = null;
+            }
+            else
+            {
+                parentnode = node;
+            }
+            
+        }
+
+
+        public void Addchildnode(MyNode childnode)
+        {
+            childnodes.Add(childnode);
+
+        }
+
+
+        public string Nodetype()
+        {
+            if (parentnode == null)
+            {
+                return "root";
+            }
+            else if (childnodes ==null || childnodes.Count <= 0)
+            {
+                return "leaf";
+            }
+            else
+            {
+                return "internal node";
+            }
+        }
+
+        public string Childnode()
         {
             StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < output.Length; i++)
+            sb.Append("[");
+            //string childnode = string.Empty;
+            //childnode = "[";
+
+
+            for (int i = 0; i < childnodes.Count; i++)
             {
-                if (i == (output.Length - 1)){
-                    sb.Append(output[i]);                   
+                if (i < childnodes.Count - 1)
+                {
+                    sb.Append(childnodes[i].value + ", ");
+                    //childnode += childnodes[i].value + ", ";
                 }
                 else
                 {
-                    sb.Append(output[i]+" ");
+                    sb.Append(childnodes[i].value);
+                    //childnode += childnodes[i].value;
                 }
             }
-            Console.WriteLine(sb.ToString());
+
+            sb.Append("]");//childnode += "]";
+
+
+            return sb.ToString();
+
         }
 
+        public int NodeDepth()
+        {
+            int nodecnt = 0;
 
+            bool parentnodechk = true;
+
+            MyNode temp_node = parentnode;
+
+            while (parentnodechk)
+            {
+
+                if (temp_node == null)
+                {
+                    break;
+                }
+                else
+                {
+                    nodecnt++;
+                }
+
+                temp_node = temp_node.parentnode;
+            }
+
+            return nodecnt;
+        }
     }
 }
---
+++ hwkang/algorithom_problem_solution/Temp_Project/my_algorithm_lib/Tree.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace my_algorithm_lib
+{
+    internal class Tree
+    {
+    }
+}
